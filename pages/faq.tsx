012925378a2cdb8fe296cc54bb95978060fadb6c
Eduardo Douglas de Melo Caetano <ecaetano@marlin.net>
2023-07-28 16:01:08 -0300

    implementando funcoes ssr e ssg para o faq

--- pages/faq.tsx
+++ pages/faq.tsx
@@ -1,7 +1,13 @@
 import Link from "../src/components/Link";
 
-//Função de SSR (Server Side Render)
+//Funções estaticas x dinamicas de SSR (Server Side Render)
+//Em modo dev ambas as funções sempre rodam a cada acesso
+
+// export async function getStaticProps() {}
+// A função getServerSideProps() {} roda a cada acesso
+
 export async function getStaticProps() {
+    // A função getStaticProps roda somente em tempo de build
     const FAQ_API_URL = 'https://gist.githubusercontent.com/omariosouto/0ceab54bdd8182cbd1a4549d32945c1a/raw/578ad1e8e5296fa048e3e7ff6b317f7497b31ad9/alura-cases-faq.json';
     const faq = await fetch(FAQ_API_URL)
             .then((response) => {
